0f15e3e169523b4b06113f64d1aaddeb28839ee4
Kirill A. Shutemov <kirill@shutemov.name>
2009-05-15 15:04:11 +0300

    config.mk: fix DATADIR
    Correct DATADIR is $(CURDIR)/data, not $(WORKDIR)/data
    
    Signed-off-by: Kirill A. Shutemov <kirill@shutemov.name>
    Signed-off-by: Alexey Gladkov <legion@altlinux.org>

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # System paths
 WORKDIR		?= $(CURDIR)/.work
 ROOTDIR		?= $(WORKDIR)/root
-DATADIR		?= $(WORKDIR)/data
+DATADIR		?= $(CURDIR)/data
 TOOLSDIR	 = $(CURDIR)/tools
 SCRIPTDIR	?= $(CURDIR)/scripts.d
 CONFIGDIR	?= $(CURDIR)/config.d
