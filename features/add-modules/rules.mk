728c8fe4ef7036f2cec8cae439248d8d9d800248
Alexey Gladkov <gladkov.alexey@gmail.com>
2009-10-02 01:41:30 +0400

    add-modules: Add preload-modules and load-modules stage
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/add-modules/rules.mk
+++ features/add-modules/rules.mk
@@ -1,8 +1,21 @@
 $(call require,depmod-image)
 
 add-modules: create
-	@echo "Adding modules ..."
-	@$(ADD_MODULE) $(MODULES_ADD)
-	@$(LOAD_MODULE) $(MODULES_LOAD)
+	@if [ -n "$(MODULES_ADD)" ]; then \
+		echo "Adding modules ..."; \
+		$(ADD_MODULE) $(MODULES_ADD); \
+	fi
 
-depmod-image: add-modules
+preload-modules: create
+	@if [ -n "$(MODULES_PRELOAD)" ]; then \
+		echo "Adding modules (preload) ..."; \
+		$(LOAD_MODULE) --pre-udev $(MODULES_PRELOAD); \
+	fi
+
+load-modules: create
+	@if [ -n "$(MODULES_LOAD)" ]; then \
+		echo "Adding modules (postload) ..."; \
+		$(LOAD_MODULE) --post-udev $(MODULES_LOAD); \
+	fi
+
+depmod-image: add-modules preload-modules load-modules
