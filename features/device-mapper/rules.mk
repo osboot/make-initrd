9aba652abb69b6279cf374dc22a6bea117614375
Kirill A. Shutemov <kirill@shutemov.name>
2009-10-02 23:02:47 +0300

    device-mapper: Load dm_mod before udev
    It needed to avoid latency between vgscan and initialization of volume
    group.
    
    Signed-off-by: Kirill A. Shutemov <kirill@shutemov.name>

--- features/device-mapper/rules.mk
+++ features/device-mapper/rules.mk
@@ -2,6 +2,6 @@ $(call require,depmod-image)
 device-mapper: create
 	@echo "Adding device-mapper support ..."
 	@$(PUT_FILE) $(DMSETUP) $(UDEV_DM_EXPORT)
-	@$(LOAD_MODULE) dm_mod
+	@$(LOAD_MODULE) --pre-udev dm_mod
 
 depmod-image: device-mapper
