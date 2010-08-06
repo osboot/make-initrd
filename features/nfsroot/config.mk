36490a898a291c0c3014c00bd0a013002fbdcc3f
Alexey Gladkov <gladkov.alexey@gmail.com>
2010-08-06 12:02:52 +0400

    nfs: preload modules only if feature enabled
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/nfsroot/config.mk
+++ features/nfsroot/config.mk
@@ -1,2 +1,2 @@
-NFS_DATADIR	?= $(FEATURESDIR)/nfsroot/data
-MODULES_PRELOAD	?= af_packet nfs
+NFS_DATADIR	= $(FEATURESDIR)/nfsroot/data
+NFS_PRELOAD	= af_packet nfs
--- features/nfsroot/rules.mk
+++ features/nfsroot/rules.mk
@@ -1,3 +1,5 @@
+MODULES_PRELOAD	+= $(NFS_PRELOAD)
+
 nfsroot:
 	@echo "Adding NFS root support ..."
 	@$(TOOLSDIR)/put-tree $(NFS_DATADIR)
