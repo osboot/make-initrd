052c33651c7af5e45192815a0c22ada00880dd9b
Alexey Gladkov <gladkov.alexey@gmail.com>
2010-08-06 13:56:43 +0400

    devmapper: Use MODULES_PRELOAD
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/devmapper/config.mk
+++ features/devmapper/config.mk
@@ -1,2 +1,5 @@
 DMSETUP		?= /sbin/dmsetup
-DM_UDEV_RULES	?= /lib/udev/rules.d/10-dm.rules /lib/udev/rules.d/11-dm-lvm.rules /lib/udev/rules.d/13-dm-disk.rules /lib/udev/rules.d/95-dm-notify.rules
+DM_UDEV_RULES	?= /lib/udev/rules.d/10-dm.rules \
+		   /lib/udev/rules.d/11-dm-lvm.rules \
+		   /lib/udev/rules.d/13-dm-disk.rules \
+		   /lib/udev/rules.d/95-dm-notify.rules
--- features/devmapper/rules.mk
+++ features/devmapper/rules.mk
@@ -1,7 +1,9 @@
+MODULES_PRELOAD += dm_mod
+
 $(call require,depmod-image)
+
 devmapper: create
 	@echo "Adding device-mapper support ..."
 	@$(TOOLSDIR)/put-file $(DMSETUP) $(DM_UDEV_RULES)
-	@$(TOOLSDIR)/load-module --pre-udev dm_mod
 
 depmod-image: devmapper
