72197dfe9643ef52534392e8c9b31c839ba62268
Alexey Gladkov <gladkov.alexey@gmail.com>
2010-08-06 13:54:17 +0400

    luks: Use MODULES_LOAD
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/luks/rules.mk
+++ features/luks/rules.mk
@@ -1,9 +1,10 @@
+MODULES_LOAD += $(LUKS_MODULES)
+
 $(call require,devmapper,depmod-image)
 
 luks: devmapper
 	@echo "Adding LUKS support ..."
 	@$(TOOLSDIR)/put-file $(CRYPTSETUP_BIN)
 	@$(TOOLSDIR)/put-tree $(LUKS_DATADIR)
-	@$(TOOLSDIR)/load-module dm-crypt $(LUKS_CIPHERS) $(LUKS_BLOCKCIPHERS) $(LUKS_HASHES)
 
 depmod-image: luks
