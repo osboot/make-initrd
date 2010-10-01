cf3916a2d3acd5ac4d1a139bee11a4cc2059d615
Alexey Shabalin <shaba@altlinux.org>
2010-10-01 13:25:49 +0400

    - auto detect theme from plymouth config - detect used plugin in theme - add plugin to image

--- features/plymouth/config.mk
+++ features/plymouth/config.mk
@@ -1,14 +1,19 @@
-PLYMOUTH_THEME  ?=
+PLYMOUTH_THEME  ?= $(shell plymouth-set-default-theme)
 PLYMOUTH_MODULES = drivers/gpu/drm/
 PLYMOUTH_DATADIR = $(FEATURESDIR)/plymouth/data
+PLYMOUTH_PLUGIN = $(shell grep "ModuleName *= *" /usr/share/plymouth/themes/$(PLYMOUTH_THEME)/$(PLYMOUTH_THEME).plymouth | sed 's/ModuleName *= *//')
 PLYMOUTH_FILES   = \
 	/bin/plymouth \
 	/sbin/plymouthd \
 	/etc/plymouth/plymouthd.conf \
+	/usr/share/plymouth/plymouthd.defaults \
+	/etc/altlinux-release \
+	/usr/share/pixmaps/altlinux.png \
 	/usr/share/plymouth/themes/details/details.plymouth \
 	/usr/share/plymouth/themes/text/text.plymouth \
 	/usr/$(LIBNAME)/plymouth/details.so \
 	/usr/$(LIBNAME)/plymouth/text.so \
 	/usr/$(LIBNAME)/plymouth/renderers/drm.so \
 	/usr/$(LIBNAME)/plymouth/renderers/frame-buffer.so \
+	/usr/$(LIBNAME)/plymouth/$(PLYMOUTH_PLUGIN).so \
 	$(wildcard /usr/share/plymouth/themes/$(PLYMOUTH_THEME)/*)
