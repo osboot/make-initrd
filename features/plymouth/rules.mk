bb7da05d07359f4991be2dbdc5984a0979a66430
Alexey Gladkov <gladkov.alexey@gmail.com>
2010-11-23 00:13:34 +0300

    plymouth: Add search of the necessary modules
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/plymouth/rules.mk
+++ features/plymouth/rules.mk
@@ -1,8 +1,9 @@
-MODULES_ADD += $(PLYMOUTH_MODULES)
-
 plymouth: create
 	@echo "Adding plymouth support ..."
 	@$(TOOLSDIR)/put-file $(PLYMOUTH_FILES)
 	@$(TOOLSDIR)/put-tree $(PLYMOUTH_DATADIR)
+	@modlist="`$(PLYMOUTH_MODULES)`"; \
+	[ -z "$$modlist" ] || \
+	   $(TOOLSDIR)/add-module $$modlist;
 
 pack: plymouth
