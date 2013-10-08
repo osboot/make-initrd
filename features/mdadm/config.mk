a460d2b27210cd37e63e37db089d09c482255837
Alexey Gladkov <gladkov.alexey@gmail.com>
2013-10-08 18:18:21 +0400

    Add support for mdadm >= 3.3
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/mdadm/config.mk
+++ features/mdadm/config.mk
@@ -1,6 +1,14 @@
 DISABLE_FEATURES += raid
 
+# mdadm < 3.3
+MDAMD_RULES_OLD = \
+	/lib/udev/rules.d/64-md-raid.rules
+
+MDAMD_RULES = \
+	/lib/udev/rules.d/63-md-raid-arrays.rules \
+	/lib/udev/rules.d/64-md-raid-assembly.rules
+
 MDADM_FILES = \
 	/sbin/mdadm \
-	/lib/udev/rules.d/64-md-raid.rules \
+	$(wildcard $(MDAMD_RULES) $(MDAMD_RULES_OLD)) \
 	/etc/mdadm.conf
