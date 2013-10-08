ad576759849492cff784d91b0b2db45c9f66a2d3
Alexey Gladkov <gladkov.alexey@gmail.com>
2013-10-08 18:52:16 +0400

    Simplify the addition of outdated files
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- initfiles.mk
+++ initfiles.mk
@@ -8,13 +8,15 @@ PUT_FILES += \
 	/lib/udev/ata_id \
 	/lib/udev/cdrom_id \
 	/lib/udev/edd_id \
-	/lib/udev/scsi_id \
+	/lib/udev/scsi_id
 
-PUT_FILES += \
-	$(shell test -f /lib/udev/vol_id && echo /lib/udev/vol_id) \
-	$(shell test -f /lib/udev/path_id && echo /lib/udev/path_id) \
-	$(shell test -f /lib/udev/usb_id && echo /lib/udev/usb_id) \
-	$(shell test -f /lib/udev/firmware && echo /lib/udev/firmware)
+UDEV_FILES_OLD = \
+	/lib/udev/vol_id \
+	/lib/udev/path_id \
+	/lib/udev/usb_id \
+	/lib/udev/firmware
+
+PUT_FILES += $(wildcard $(UDEV_FILES_OLD))
 
 # System utilities
 PUT_FILES += \
