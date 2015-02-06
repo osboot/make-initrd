a3076d2a032da636c6978fe433d32143034cee5e
Anton Farygin <rider@altlinux.com>
2015-02-06 16:19:40 +0300

    add udev rules for new plymouth

--- features/plymouth/rules.mk
+++ features/plymouth/rules.mk
@@ -2,7 +2,7 @@ MODULES_ADD += $(shell $(PLYMOUTH_MODULES))
 
 plymouth: create
 	@echo "Adding plymouth support ..."
-	@put-file "$(ROOTDIR)" $(PLYMOUTH_FILES)
+	@put-file "$(ROOTDIR)" $(PLYMOUTH_FILES) $(PLYMOUTH_RULES)
 	@put-tree "$(ROOTDIR)" $(PLYMOUTH_DATADIR)
 
 pack: plymouth
