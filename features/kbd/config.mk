78c39cf66a89b5e8d1f6bb3457b87e81969a78fe
Alexey Gladkov <gladkov.alexey@gmail.com>
2015-07-05 21:36:25 +0300

    Add kbd feature
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

---
+++ features/kbd/config.mk
@@ -0,0 +1,12 @@
+KBD_DATADIR = $(FEATURESDIR)/kbd/data
+KBD_FILES = \
+	/bin/kbd_mode \
+	/bin/setfont \
+	/bin/loadkeys \
+	/lib/kbd/keymaps/i386 \
+	/lib/kbd/keymaps/include \
+	/lib/kbd/keymaps/consolefonts
+
+KBD_CONFIGS = \
+	/etc/sysconfig/consolefont \
+	/etc/sysconfig/keyboard
---
+++ features/kbd/rules.mk
@@ -0,0 +1,8 @@
+kbd:
+	@echo "Adding kbd support ..."
+	@put-file "$(ROOTDIR)" $(KBD_FILES)
+	@put-tree "$(ROOTDIR)" $(KBD_DATADIR)
+	@mkdir -p -- "$(ROOTDIR)/etc/sysconfig"
+	@cat $(KBD_CONFIGS) > "$(ROOTDIR)/etc/sysconfig/console"
+
+pack: kbd
