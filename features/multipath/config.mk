e66da1257f531759f66d2223333c54e0f4da4251
Alexey Gladkov <gladkov.alexey@gmail.com>
2016-06-23 18:35:21 +0200

    Fix formating
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/multipath/config.mk
+++ features/multipath/config.mk
@@ -1,11 +1,9 @@
-MULTIPATH_PRELOAD	= dm-multipath
-MULTIPATH_MODULES	= scsi_dh scsi_dh_alua scsi_dh_emc \
-			  scsi_dh_hp_sw scsi_dh_rdac
-
-MULTIPATH_BIN		= /sbin/multipath /sbin/kpartx \
-			  /lib/udev/kpartx_id
-
-MULTIPATH_CONF		= /etc/multipath.conf
-
-MULTIPATH_DATA          = /lib/udev/rules.d/40-kpartx.rules \
-			  /lib/udev/rules.d/40-multipath.rules
+MULTIPATH_PRELOAD = dm-multipath
+MULTIPATH_MODULES = \
+	scsi_dh scsi_dh_alua scsi_dh_emc \
+	scsi_dh_hp_sw scsi_dh_rdac
+MULTIPATH_BIN     = /sbin/multipath /sbin/kpartx /lib/udev/kpartx_id
+MULTIPATH_CONF    = /etc/multipath.conf
+MULTIPATH_DATA    = \
+	/lib/udev/rules.d/40-kpartx.rules \
+	/lib/udev/rules.d/40-multipath.rules
--- features/usb/rules.mk
+++ features/usb/rules.mk
@@ -1,3 +1,4 @@
-MODULES_ADD	+= usb-storage sdhci sdhci-pci \
-		   usbhid hid-apple \
-		   ehci-hcd ohci-hcd uhci-hcd
+MODULES_ADD	+= \
+	usb-storage sdhci sdhci-pci \
+	usbhid hid-apple \
+	ehci-hcd ohci-hcd uhci-hcd
