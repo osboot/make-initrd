a471833543aa46fa5639928f13407a90d7367960
Valery Inozemtsev <shrek@altlinux.ru>
2016-11-03 16:14:03 +0300

    multipath:  - added path selector modules  - added kpartx

--- features/multipath/config.mk
+++ features/multipath/config.mk
@@ -1,11 +1,13 @@
-MULTIPATH_PRELOAD	= scsi_dh_alua scsi_dh_emc scsi_dh_rdac dm-multipath
+MULTIPATH_PRELOAD	= scsi_dh_alua scsi_dh_emc scsi_dh_rdac \
+			  dm-multipath dm-service-time dm-queue-length dm-round-robin
 
-MULTIPATH_BIN		= /sbin/multipathd /sbin/multipath \
+MULTIPATH_BIN		= /sbin/multipathd /sbin/multipath /sbin/kpartx \
 			  /$(LIBNAME)/multipath /$(LIBNAME)/libmultipath.so.0
 
 MULTIPATH_CONF		= /etc/multipath.conf /etc/multipath
 
 MULTIPATH_DATA          = /lib/udev/rules.d/11-dm-mpath.rules \
-			  /lib/udev/rules.d/56-multipath.rules
+			  /lib/udev/rules.d/56-multipath.rules \
+			  /lib/udev/rules.d/66-kpartx.rules
 
 MULTIPATH_DATADIR	= $(FEATURESDIR)/multipath/data
