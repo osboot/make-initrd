e6c5031c3531f0b420de5da18f27070b6a3eff13
Alexey Gladkov <gladkov.alexey@gmail.com>
2017-03-21 11:37:43 +0100

    multipath: Add path selector modules, kpartx
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/multipath/config.mk
+++ features/multipath/config.mk
@@ -1,9 +1,18 @@
-MULTIPATH_PRELOAD = dm-multipath
+MULTIPATH_PRELOAD = \
+	dm-multipath \
+	dm-queue-length \
+	dm-round-robin \
+	dm-service-time \
+	scsi_dh_alua \
+	scsi_dh_emc \
+	scsi_dh_rdac
+
 MULTIPATH_MODULES = \
 	scsi_dh scsi_dh_alua scsi_dh_emc \
 	scsi_dh_hp_sw scsi_dh_rdac
+
 MULTIPATH_BIN     = /sbin/multipath /sbin/kpartx /lib/udev/kpartx_id
-MULTIPATH_CONF    = /etc/multipath.conf
+MULTIPATH_CONF    = /etc/multipath.conf /etc/multipath
 MULTIPATH_DATA    = \
-	/lib/udev/rules.d/40-kpartx.rules \
-	/lib/udev/rules.d/40-multipath.rules
+	$(wildcard /lib/udev/rules.d/*-kpartx.rules) \
+	$(wildcard /lib/udev/rules.d/*-multipath.rules)
