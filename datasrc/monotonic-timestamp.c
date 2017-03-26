fcfffa00ee38a4ab68e9f0b2122d4fdcfca930f3
Alexey Gladkov <gladkov.alexey@gmail.com>
2017-03-26 16:39:11 +0200

    Add timestamp to records
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

---
+++ datasrc/monotonic-timestamp.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(void)
+{
+	struct timespec tp = { 0 };
+	clock_gettime(CLOCK_MONOTONIC, &tp);
+	printf("[%5lu.%06lu] ", (unsigned long) tp.tv_sec, tp.tv_nsec / 1000);
+	return 0;
+}
