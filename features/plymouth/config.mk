253b6eb46ad3da134e770a60b8133adebbd8f69c
Alexey Gladkov <gladkov.alexey@gmail.com>
2019-03-01 11:16:34 +0100

    Feature plymouth: Handle ImageDir
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/plymouth/config.mk
+++ features/plymouth/config.mk
@@ -1,19 +1,17 @@
 PLYMOUTH_THEME  ?= $(shell plymouth-set-default-theme 2>/dev/null ||:)
 PLYMOUTH_MODULES = $(FEATURESDIR)/plymouth/get-modules
 PLYMOUTH_DATADIR = $(FEATURESDIR)/plymouth/data
-PLYMOUTH_PLUGIN  = $(shell $(FEATURESDIR)/plymouth/get-plugin $(PLYMOUTH_THEME))
-PLYMOUTH_FILES   = \
+PLYMOUTH_FILES = \
 	/bin/plymouth \
 	/sbin/plymouthd \
 	/etc/plymouth/plymouthd.conf \
 	/usr/share/plymouth/plymouthd.defaults \
 	/etc/altlinux-release \
+	/var/lib/plymouth/boot-duration \
 	/usr/share/plymouth/themes/details/details.plymouth \
 	/usr/share/plymouth/themes/text/text.plymouth \
 	/usr/$(LIBNAME)/plymouth/details.so \
 	/usr/$(LIBNAME)/plymouth/text.so \
 	/usr/$(LIBNAME)/plymouth/renderers/drm.so \
 	/usr/$(LIBNAME)/plymouth/renderers/frame-buffer.so \
-	/usr/$(LIBNAME)/plymouth/$(PLYMOUTH_PLUGIN).so \
-	$(wildcard /usr/share/plymouth/themes/$(PLYMOUTH_THEME)/*) \
-	/var/lib/plymouth/boot-duration
+	$(shell $(FEATURESDIR)/plymouth/get-theme-files $(PLYMOUTH_THEME))
