608ca32839bf6187c26e751ce399afedcc5830dd
Alexey Gladkov <gladkov.alexey@gmail.com>
2019-04-17 15:40:01 +0200

    Feature kbd: Add gzip/bzip2/xz if files are compressed
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/kbd/config.mk
+++ features/kbd/config.mk
@@ -1,11 +1,15 @@
 KBD_DATADIR = $(FEATURESDIR)/kbd/data
+
+KBD_FONTS_DIR   = /lib/kbd/consolefonts
+KBD_KEYMAPS_DIR = /lib/kbd/keymaps
+
 KBD_FILES = \
 	/bin/kbd_mode \
 	/bin/setfont \
 	/bin/loadkeys \
-	/lib/kbd/keymaps/i386 \
-	/lib/kbd/keymaps/include \
-	/lib/kbd/consolefonts
+	$(KBD_KEYMAPS_DIR)/i386 \
+	$(KBD_KEYMAPS_DIR)/include \
+	$(KBD_FONTS_DIR)
 
 KBD_CONFIGS = \
 	/etc/sysconfig/consolefont \
--- features/kbd/rules.mk
+++ features/kbd/rules.mk
@@ -1,5 +1,9 @@
 kbd:
 	@$(MSG) "Adding kbd support ..."
+	@for s in gzip:gz bzip2:bz2 xz:xz; do \
+		[ -z "$$(find "$(KBD_FONTS_DIR)" "$(KBD_KEYMAPS_DIR)" -type f -name "*.$${s#*:}" -print -quit)" ] || \
+			put-file "$(ROOTDIR)" `which $${s%:*}`; \
+	done
 	@put-file "$(ROOTDIR)" $(KBD_FILES)
 	@put-tree "$(ROOTDIR)" $(KBD_DATADIR)
 	@mkdir -p -- "$(ROOTDIR)/etc/sysconfig"
