42b919b5264cca412c552c4830bca421ea3a6806
Alexey Gladkov <gladkov.alexey@gmail.com>
2019-10-25 12:17:07 +0200

    runtime: Re-implement ueventd in shell
    * Rewrite in shell to improve runtime troubleshooting;
    * Use inotifyd from busybox to avoid polling;
    * Support of multiple queues.
    
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- datasrc/uevent-epoll.h
+++
@@ -1,9 +0,0 @@
-#ifndef _UEVENT_EPOLL_H_
-#define _UEVENT_EPOLL_H_
-
-#include <sys/epoll.h>
-
-void epollin_add(int fd_ep, int fd);
-void epollin_remove(int fd_ep, int fd);
-
-#endif /* _UEVENT_EPOLL_H_ */
--- datasrc/uevent-worker.h
+++
@@ -1,8 +0,0 @@
-#ifndef _UEVENT_WORKER_H_
-#define _UEVENT_WORKER_H_
-
-#include <unistd.h>
-
-pid_t spawn_worker(char *path, char **argv);
-
-#endif /* _UEVENT_WORKER_H_ */
