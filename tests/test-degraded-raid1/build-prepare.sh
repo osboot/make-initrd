bd129148b28bb28c204f52345168d48f47c28c9b
Alexey Gladkov <gladkov.alexey@gmail.com>
2020-03-03 10:47:57 +0100

    tests: Do not hardcode device names
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- tests/test-degraded-raid1/build-prepare.sh
+++ tests/test-degraded-raid1/build-prepare.sh
@@ -2,8 +2,11 @@
 
 read -r testcase < "$HOME/src/.tests/cur" ||:
 
+disk1="$(blkid --output 'device' --match-token UUID_SUB="82dd176e-ade8-1600-194d-7f7d36f22fb8")"
+disk2="$(blkid --output 'device' --match-token UUID_SUB="843dc522-ab96-32bb-8143-b02904fa2b2f")"
+
 mdadm --assemble \
 	-c "$HOME/src/tests/$testcase/mdadm.conf" \
-	/dev/md0 /dev/sdb /dev/sdc
+	/dev/md0 "$disk1" "$disk2"
 
 mount /dev/md0 "$HOME/rootfs"
