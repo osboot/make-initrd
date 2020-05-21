42aaf0e46265e105da4d7150368cab64206cba6f
Alexey Gladkov <gladkov.alexey@gmail.com>
2020-05-21 15:21:57 +0200

    Feature pipeline: Add rules for cdrom and useful kernel modules
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/pipeline/config.mk
+++ features/pipeline/config.mk
@@ -1,1 +1,7 @@
 PIPELINE_DATADIR = $(FEATURESDIR)/pipeline/data
+
+PIPELINE_FILES = \
+	/lib/udev/rules.d/60-cdrom_id.rules \
+	/lib/udev/cdrom_id
+
+PIPELINE_MODULES = isofs squashfs overlay
--- features/pipeline/rules.mk
+++ features/pipeline/rules.mk
@@ -1,5 +1,8 @@
+MODULES_TRY_ADD += $(PIPELINE_MODULES)
+
 pipeline: create
 	@$(MSG) "Adding pipeline support ..."
+	@put-file "$(ROOTDIR)" $(PIPELINE_FILES)
 	@put-tree "$(ROOTDIR)" $(PIPELINE_DATADIR)
 
 pack: pipeline
