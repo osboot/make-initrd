b298e2eac1d61a04babac002d9bb8f6e905633fc
Alexey Gladkov <gladkov.alexey@gmail.com>
2020-10-05 13:28:38 +0200

    runtime: Use nanoseconds to serialize udev events
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- datasrc/monotonic-timestamp/monotonic-timestamp.c
+++ datasrc/monotonic-timestamp/monotonic-timestamp.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 
 int
-main(void)
+main(int argc, char **argv)
 {
 	struct timespec tp = { 0 };
 	clock_gettime(CLOCK_MONOTONIC, &tp);
+	if (argc == 2 && !strcmp(argv[1], "raw")) {
+		printf("%06lu.%07lu", (unsigned long) tp.tv_sec, tp.tv_nsec);
+		return 0;
+	}
 	printf("[%5lu.%06lu] ", (unsigned long) tp.tv_sec, tp.tv_nsec / 1000);
 	return 0;
 }
