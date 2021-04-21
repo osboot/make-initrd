6cbfe8a9519999f20509802e70d42aca7891575c
Alexey Gladkov <gladkov.alexey@gmail.com>
2021-04-21 12:55:32 +0200

    Feature plymouth: Improve portability
    In altlinux, fedora, and ubuntu, the plymouth files are located in
    different locations.
    
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/plymouth/config.mk
+++ features/plymouth/config.mk
@@ -1,24 +1,26 @@
 $(call feature-requires,depmod-image)
 
 PLYMOUTH_DEFAULT_THEME := $(shell plymouth-set-default-theme 2>/dev/null ||:)
+PLYMOUTH_LIBDIR := $(shell $(FEATURESDIR)/plymouth/bin/get-libdir)
+PLYMOUTH_FONT := $(shell fc-match -f '%{file}\n' 'DejaVuSans')
 
 PLYMOUTH_THEME  ?= $(PLYMOUTH_DEFAULT_THEME)
 PLYMOUTH_GET_MODULES = $(FEATURESDIR)/plymouth/bin/get-modules
 PLYMOUTH_DATADIR = $(FEATURESDIR)/plymouth/data
 PLYMOUTH_PROGS = plymouth plymouthd
 PLYMOUTH_FILES = \
-	$(SYSCONFDIR)/plymouth/plymouthd.conf \
+	$(wildcard $(SYSCONFDIR)/plymouth/plymouthd.conf) \
 	$(wildcard $(SYSCONFDIR)/*-release) \
-	/var/lib/plymouth/boot-duration \
-	$(DATADIR)/plymouth/plymouthd.defaults \
+	$(wildcard /var/lib/plymouth/boot-duration) \
+	$(wildcard $(DATADIR)/plymouth/plymouthd.defaults) \
 	$(DATADIR)/plymouth/themes/details/details.plymouth \
 	$(DATADIR)/plymouth/themes/text/text.plymouth \
-	$(DATADIR)/fonts/ttf/dejavu/DejaVuSans.ttf \
-	$(LIBDIR)/plymouth/details.so \
-	$(LIBDIR)/plymouth/text.so \
-	$(LIBDIR)/plymouth/label.so \
-	$(LIBDIR)/plymouth/renderers/drm.so \
-	$(LIBDIR)/plymouth/renderers/frame-buffer.so \
+	$(PLYMOUTH_LIBDIR)/details.so \
+	$(PLYMOUTH_LIBDIR)/text.so \
+	$(PLYMOUTH_LIBDIR)/label.so \
+	$(PLYMOUTH_LIBDIR)/renderers/drm.so \
+	$(PLYMOUTH_LIBDIR)/renderers/frame-buffer.so \
+	$(PLYMOUTH_FONT) \
 	$(shell env \
 		"LIBDIR=$(LIBDIR)" \
 		"DATADIR=$(DATADIR)" \
