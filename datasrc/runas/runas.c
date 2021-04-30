78dd52f4c18df97f043a95de4ce11a2167e79a9d
Alexey Gladkov <gladkov.alexey@gmail.com>
2021-04-30 12:10:12 +0200

    runas: Replace error() by more portable err()
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- datasrc/runas/runas.c
+++ datasrc/runas/runas.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
-#include <errno.h>
-#include <error.h>
+#include <err.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-extern const char *__progname;
+#include <libgen.h>
 
 int
 main (int ac, char *av[])
 {
 	if (ac < 3)
-		fprintf (stderr, "usage: %s program [arguments]\n", __progname);
+		fprintf (stderr, "usage: %s program [arguments]\n", basename(av[0]));
 	else
 	{
 		const char *path = av[2];
 
 		av[2] = av[1];
 		execvp (path, av + 2);
-		error (EXIT_FAILURE, errno, "%s", path);
+		err(EXIT_FAILURE, "%s", path);
 	}
 	return EXIT_FAILURE;
 }
