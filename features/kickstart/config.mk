d8a425fcf1b8bc0f4c18a82ecde6348faaea39b9
Alexey Gladkov <gladkov.alexey@gmail.com>
2021-05-08 11:23:08 +0200

    Feature kickstart: Add more utilities that are used in kickstart
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/kickstart/config.mk
+++ features/kickstart/config.mk
@@ -1,7 +1,7 @@
 $(call feature-requires,devmapper luks lvm mdadm modules-filesystem system-glibc network)
 
 KICKSTART_PROGS  = sfdisk wipefs blkid findmnt mkswap mount mountpoint \
-		   rsync wget tar unzip cpio env sha256sum
+		   rsync wget tar unzip cpio env sha256sum eject halt reboot
 
 KICKSTART_PROGS_PATTERNS = \
 			   */mkfs.btrfs \
