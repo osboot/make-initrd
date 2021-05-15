2b754a3625e82256a8bdda4bac87821a5a0a89f8
Alexey Gladkov <gladkov.alexey@gmail.com>
2021-05-15 13:18:06 +0200

    Feature kickstart: Add mkfs.{btrfs,ext4} to feature requires
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/kickstart/config.mk
+++ features/kickstart/config.mk
@@ -1,10 +1,10 @@
 $(call feature-requires,devmapper luks lvm mdadm modules-filesystem system-glibc network)
 
 KICKSTART_PROGS  = sfdisk wipefs blkid findmnt mkswap mount mountpoint chroot \
-		   rsync wget tar unzip cpio env sha256sum eject halt reboot
+		   rsync wget tar unzip cpio env sha256sum eject halt reboot \
+		   btrfs mkfs.btrfs mkfs.ext4
 
 KICKSTART_PROGS_PATTERNS = \
-			   */mkfs.btrfs \
 			   */mkfs.ext* \
 			   */mkfs.xfs \
 			   */mkfs.fat \
