f123c1ca65a1a7eb0c38f6948d68ce31bfa82bdc
Petr Mikhalicin <pmikhalicin@rutoken.ru>
2021-07-12 16:38:28 +0300

    Feature sshfs: Detect smart-card feature dependency according ssh config file
    Signed-off-by: Petr Mikhalicin <pmikhalicin@rutoken.ru>

--- features/sshfsroot/config.mk
+++ features/sshfsroot/config.mk
@@ -8,3 +8,11 @@ SSHFS_KNOWN_HOSTS ?=
 SSHFS_CONFIG ?=
 SSHFS_KEY ?=
 SSHFS_KEYS ?=
+
+ifdef SSHFS_CONFIG
+    SMART_CARD_PKCS11_MODULE ?= $(shell grep -w PKCS11Provider "$(SSHFS_CONFIG)" | tail -n1 | sed -e 's/[[:space:]]\+/ /g' -e 's/^ //' | cut -d" " -f 2)
+endif
+
+ifdef SMART_CARD_PKCS11_MODULE
+    $(call feature-requires,smart-card)
+endif
