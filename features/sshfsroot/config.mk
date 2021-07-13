01a407d3269d3fd6f12c3697e645068df9ec189b
Alexey Gladkov <gladkov.alexey@gmail.com>
2021-07-13 16:55:18 +0200

    Feature sshfsroot: Fix SMART_CARD_PKCS11_MODULE detection
    - Ignore commented PKCS11Provider.
    - Show an error if more than one PKCS11Provider specified.
    
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/sshfsroot/config.mk
+++ features/sshfsroot/config.mk
@@ -10,10 +10,13 @@ SSHFS_KEY ?=
 SSHFS_KEYS ?=
 
 ifdef SSHFS_CONFIG
-SSHFS_PKCS11_MODULE := $(shell $(FEATURESDIR)/sshfsroot/bin/get-pkcs11-module "$(SSHFS_CONFIG)")
+SSHFS_PKCS11_MODULES := $(shell $(FEATURESDIR)/sshfsroot/bin/get-pkcs11-module "$(SSHFS_CONFIG)")
+ifneq "$(words $(sort $(SSHFS_PKCS11_MODULES)))" "1"
+$(error Different PKCS11 providers are not supported for sshfsroot)
+endif
 endif
 
-ifdef SSHFS_PKCS11_MODULE
-SMART_CARD_PKCS11_MODULE = $(SSHFS_PKCS11_MODULE)
+ifdef SSHFS_PKCS11_MODULES
+SMART_CARD_PKCS11_MODULE = $(sort $(SSHFS_PKCS11_MODULES))
 $(call feature-requires,smart-card)
 endif
