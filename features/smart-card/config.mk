70c91953eb735db49099714c0f2f4857db2d0486
Alexey Gladkov <gladkov.alexey@gmail.com>
2021-07-13 16:14:08 +0200

    Feature smard-card: Make /etc/opensc optional and optional config
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/smart-card/config.mk
+++ features/smart-card/config.mk
@@ -2,8 +2,9 @@ $(call feature-requires,usb)
 
 SMART_CARD_BIN		?= pcscd pkcs11-tool opensc-tool pkcs15-tool pkcs15-init pkcs15-crypt pcsc_scan
 SMART_CARD_DATADIR	?= $(FEATURESDIR)/smart-card/data
-SMART_CARD_FILES	?= $(SYSCONFDIR)/opensc\
-			   $(SYSCONFDIR)/reader.conf.d\
+SMART_CARD_FILES	?= $(wildcard $(SYSCONFDIR)/opensc) \
+			   $(wildcard $(SYSCONFDIR)/opensc.conf) \
+			   $(SYSCONFDIR)/reader.conf.d \
 			   $(LIBDIR)/pcsc
 
 SMART_CARD_PKCS11_MODULE ?= opensc-pkcs11.so
