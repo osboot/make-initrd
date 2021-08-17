0e9419d4fd4198ed04acae1fd92d5dae15772276
Михалицын Петр <pmikhalicin@rutoken.ru>
2021-08-17 11:24:32 +0300

    guess smart-card: Fix typos and move guessing GUESS_NET_IFACE to net module
    Signed-off-by: Михалицын Петр <pmikhalicin@rutoken.ru>

--- guess/smart-card/config.mk
+++ guess/smart-card/config.mk
@@ -1,15 +1,3 @@
 ifeq "$(call if-recently-activated-feature,sshfsroot)" "sshfsroot"
-GUESS_MODULES += sshfsroot
-endif
-
-ifeq "$(call if-active-feature,sshfsroot)/$(call if-module-guessable,net)" "sshfsroot/net"
-
-ifeq "$(GUESS_NET_IFACE)" ''
-GUESS_MODULES += net
-endif
-
-ifeq "$(GUESS_NET_IFACE)" ''
-GUESS_NET_IFACE = all
-endif
-
+GUESS_MODULES += smart-card
 endif
