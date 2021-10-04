9de1abb36add79f3ff77738e476a6094b4427db7
Anton Midyukov <antohami@altlinux.org>
2021-10-04 23:35:27 +0700

    Add feature sbc for single board computers support
    For not all single board computers, make-initrd can build an initrd
    to boot successfully.
    
    Signed-off-by: Anton Midyukov <antohami@altlinux.org>

---
+++ features/modules-sbc/rules.mk
@@ -0,0 +1,17 @@
+MODULES_TRY_ADD += \
+	drivers/bus         \
+	drivers/dma         \
+	drivers/i2c         \
+	drivers/mfd         \
+	drivers/mmc         \
+	drivers/nvmem       \
+	drivers/phy         \
+	drivers/regulator   \
+	drivers/reset       \
+	drivers/soc         \
+	drivers/usb/host    \
+	drivers/usb/storage \
+	drivers/usb/dwc2    \
+	drivers/usb/dwc3    \
+	drivers/usb/musb    \
+	drivers/usb/phy
