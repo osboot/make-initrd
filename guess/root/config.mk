ca237497914032c4e24be2a56a4d49c8a3c5d3f4
Alexey Gladkov <gladkov.alexey@gmail.com>
2022-09-23 18:19:45 +0200

    Detect separate /usr partition
    On modern systems, almost the entire installed operating system is
    located in /usr and /bin, /sbin and /lib point inside it.
    
    ```
    /
    |-- etc
    |-- usr
    |   |-- bin
    |   |-- sbin
    |   |-- lib
    |   `-- lib64
    |-- run
    |-- var
    |-- bin -> usr/bin
    |-- sbin -> usr/sbin
    |-- lib -> usr/lib
    `-- lib64 -> usr/lib64
    ```
    
    If /usr is on a separate partition, we need to mount it in the same way
    as / (root partition). We are ready for it. The only thing missing is
    the auto-detection of this situation.
    
    Link: https://fedoraproject.org/wiki/Features/UsrMove
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- guess/root/config.mk
+++ guess/root/config.mk
@@ -1,6 +1,1 @@
-#
-# Search for options:
-#   comment="x-initrd-mount"
-#   x-initrd-mount
-#
-GUESS_MOUNTPOINTS += / $(shell LIBMOUNT_FSTAB=$(FSTAB) findmnt -s -O x-initrd-mount -no TARGET 2>/dev/null ||:)
+GUESS_MOUNTPOINTS ?=
