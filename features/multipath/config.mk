03ef73cc3e0580d79be0e2ef0910ab7553335071
Andrew A. Vasilyev <andy@altlinux.org>
2022-09-26 15:09:35 +0300

    Feature multipath: Add more rules and utils for FC multipath.
    Link: https://github.com/osboot/make-initrd/pull/28
    Signed-off-by: Andrew A. Vasilyev <andy@altlinux.org>
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/multipath/config.mk
+++ features/multipath/config.mk
@@ -9,12 +9,20 @@ MULTIPATH_PRELOAD = \
 	scsi_dh_emc \
 	scsi_dh_rdac
 
-MULTIPATH_PROGS   = multipathd multipath kpartx
+MULTIPATH_PROGS   = multipathd multipath kpartx sg_inq
 MULTIPATH_CONF    = \
 	$(SYSCONFDIR)/multipath.conf \
-	$(SYSCONFDIR)/multipath
+	$(SYSCONFDIR)/multipath \
+	$(SYSCONFDIR)/multipath/bindings \
+	$(SYSCONFDIR)/multipath/wwids
 MULTIPATH_DATADIR = \
 	$(FEATURESDIR)/multipath/data
 MULTIPATH_RULES   = \
+	*-dm-mpath.rules \
+	*-dm-parts.rules \
+	*-fc-wwpn-id.rules \
 	*-kpartx.rules \
-	*-multipath.rules
+	*-multipath.rules \
+	*-scsi-sg3*.rules \
+	*-usb-blacklist.rules
+MULTIPATH_FILES = /lib/udev/fc_wwpn_id
