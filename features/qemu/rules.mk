c6addafd4e4cf2ff2433e72eabfaef6def34f367
Gleb Fotengauer-Malinovskiy <glebfm@altlinux.org>
2022-09-30 17:58:49 +0000

    Feature qemu: try to add e1000e module
    e1000e is the default nic model for pc-q35 >= 2.12 machines (see qemu
    commit v2.12.0-rc0~16^2~66).
    
    Signed-off-by: Gleb Fotengauer-Malinovskiy <glebfm@altlinux.org>

--- features/qemu/rules.mk
+++ features/qemu/rules.mk
@@ -1,4 +1,4 @@
 MODULES_TRY_ADD += \
-	8139cp ahci ata_generic ata_piix cdrom e100 e1000 \
+	8139cp ahci ata_generic ata_piix cdrom e100 e1000 e1000e \
 	ne2k_pci pata_acpi pcnet32 sr_mod \
 	virtio.*
