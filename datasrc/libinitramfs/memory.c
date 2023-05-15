osboot/make-initrd

commit 9e3834d40911a59f591aaec7370502464c13d26a

Move memory functions to the library
Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

datasrc/libinitramfs/memory.c
@@ -8,9 +8,10 @@
 #include <error.h>
 #include <limits.h>
 
-#include "ueventd.h"
+#include "rd/memory.h"
+#include "rd/logging.h"
 
-void *xcalloc(size_t nmemb, size_t size)
+void *rd_calloc_or_die(size_t nmemb, size_t size)
 {
 	void *r = calloc(nmemb, size);
 	if (!r)
@@ -19,7 +20,16 @@ void *xcalloc(size_t nmemb, size_t size)
 	return r;
 }
 
-char *xasprintf(char **ptr, const char *fmt, ...)
+void *rd_malloc_or_die(size_t size)
+{
+	void *r = malloc(size);
+	if (!r)
+		rd_fatal("malloc: allocating %lu bytes: %m",
+		         (unsigned long) size);
+	return r;
+}
+
+char *rd_asprintf_or_die(char **ptr, const char *fmt, ...)
 {
 	va_list arg;
 

datasrc/environ/Makefile.mk
@@ -3,4 +3,7 @@ environ_SRCS = datasrc/environ/environ.c
 
 PROGS += environ
 
+environ_CFLAGS = -D_GNU_SOURCE -Idatasrc/libinitramfs
+environ_LIBS = -L$(dest_data_libdir) -linitramfs
+
 environ_SYMLINKS += $(dest_data_bindir)/showenv

datasrc/environ/environ.c
@@ -9,6 +9,8 @@
 #include <fnmatch.h>
 #include <fcntl.h>
 
+#include "rd/memory.h"
+
 #ifndef _GNU_SOURCE
 extern char **environ;
 #endif
@@ -183,12 +185,7 @@ unset_environ(char **envp, char *patterns, int invert_match)
 		last++;
 	}
 
-	envname = calloc(envp_sz, sizeof(ssize_t));
-
-	if (!envname) {
-		fprintf(stderr, "ERROR: calloc: %m\n");
-		exit(1);
-	}
+	envname = rd_calloc_or_die(envp_sz, sizeof(ssize_t));
 
 	e = last;
 	i = envp_sz;
@@ -213,12 +210,7 @@ next:
 		i--;
 	}
 
-	env = malloc(sizeof(char) * (max + 1));
-
-	if (!env) {
-		fprintf(stderr, "ERROR: malloc: %m\n");
-		exit(1);
-	}
+	env = rd_malloc_or_die(sizeof(char) * (max + 1));
 
 	pattern = s = patterns;
 	next = NULL;

datasrc/libinitramfs/Makefile.mk
@@ -1,6 +1,7 @@
 libinitramfs_DEST = $(dest_data_libdir)/libinitramfs.so
 libinitramfs_SRCS = \
 	datasrc/libinitramfs/logging.c \
+	datasrc/libinitramfs/memory.c \
 	$(NULL)
 
 libinitramfs_CFLAGS = \

datasrc/libinitramfs/rd/memory.h
@@ -1,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#ifndef __RD_MEMORY_H__
+#define __RD_MEMORY_H__
+
+#include <stdlib.h>
+
+void *rd_calloc_or_die(size_t nmemb, size_t size)          __attribute__((alloc_size(1, 2)));
+void *rd_malloc_or_die(size_t size)                        __attribute__((alloc_size(1)));
+char *rd_asprintf_or_die(char **ptr, const char *fmt, ...) __attribute__((format(printf, 2, 3)));
+
+#endif /* __RD_MEMORY_H__ */

datasrc/ueventd/Makefile.mk
@@ -1,6 +1,5 @@
 ueventd_DEST = $(dest_data_sbindir)/ueventd
 ueventd_SRCS = \
-	datasrc/ueventd/memory.c \
 	datasrc/ueventd/path.c \
 	datasrc/ueventd/process.c \
 	datasrc/ueventd/queue-processor.c \

datasrc/ueventd/queue-processor.c
@@ -12,6 +12,7 @@
 #include <fcntl.h>
 #include <errno.h>
 
+#include "rd/memory.h"
 #include "ueventd.h"
 
 static void event_handler(struct watch *queue, char *path) __attribute__((nonnull(1, 2)));
@@ -92,13 +93,13 @@ void process_events(struct watch *queue)
 
 	char *numenv = NULL;
 
-	xasprintf(&numenv, "SESSION=%lu", session);
+	rd_asprintf_or_die(&numenv, "SESSION=%lu", session);
 	putenv(numenv);
 
 	char *srcdir, *dstdir;
 
-	xasprintf(&srcdir, "%s/%s", filter_dir, queue->q_name);
-	xasprintf(&dstdir, "%s/%s", uevent_dir, queue->q_name);
+	rd_asprintf_or_die(&srcdir, "%s/%s", filter_dir, queue->q_name);
+	rd_asprintf_or_die(&dstdir, "%s/%s", uevent_dir, queue->q_name);
 
 	move_files(srcdir, dstdir);
 

datasrc/ueventd/ueventd.c
@@ -19,6 +19,7 @@
 #include <getopt.h>
 #include <errno.h>
 
+#include "rd/memory.h"
 #include "ueventd.h"
 
 #define default_logfile "/var/log/ueventd.log"
@@ -98,7 +99,7 @@ int watch_path(int inotifyfd, const char *dir, const char *name, uint32_t mask, 
 	struct watch *new = NULL;
 	char *path = NULL;
 
-	xasprintf(&path, "%s/%s", dir, name);
+	rd_asprintf_or_die(&path, "%s/%s", dir, name);
 
 	int wfd = add_queue_dir(inotifyfd, path, mask);
 	if (wfd < 0)
@@ -109,7 +110,7 @@ int watch_path(int inotifyfd, const char *dir, const char *name, uint32_t mask, 
 		goto fail;
 	}
 
-	new = xcalloc(1, sizeof(*new) + sizeof(new->q_name[0]) * (strlen(name) + 1));
+	new = rd_calloc_or_die(1, sizeof(*new) + sizeof(new->q_name[0]) * (strlen(name) + 1));
 
 	strcpy(new->q_name, name);
 
@@ -168,7 +169,7 @@ void watch_pauses(int inotifyfd)
 {
 	char *path = NULL;
 
-	xasprintf(&path, "%s/queue/pause", uevent_confdb);
+	rd_asprintf_or_die(&path, "%s/queue/pause", uevent_confdb);
 
 	if (watch_path(inotifyfd, path, ".", EV_PAUSE_MASK, F_PAUSE_DIR) < 0)
 		exit(EXIT_FAILURE);
@@ -182,7 +183,7 @@ void apply_pause(void)
 	DIR *dir;
 	char *path = NULL;
 
-	xasprintf(&path, "%s/queue/pause", uevent_confdb);
+	rd_asprintf_or_die(&path, "%s/queue/pause", uevent_confdb);
 
 	dir = xopendir(path);
 	pause_all = 0;

commit 1e9d8248f58143937b15f794b8d3bace1df8de81

libinitramfs: Do not use explicit type conversion
Fixes: 9e3834d4 ("Move memory functions to the library")
Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

datasrc/libinitramfs/memory.c
@@ -15,8 +15,7 @@ void *rd_calloc_or_die(size_t nmemb, size_t size)
 {
 	void *r = calloc(nmemb, size);
 	if (!r)
-		rd_fatal("calloc: allocating %lu*%lu bytes: %m",
-		         (unsigned long) nmemb, (unsigned long) size);
+		rd_fatal("calloc: allocating %zu*%zu bytes: %m", nmemb, size);
 	return r;
 }
 
@@ -24,8 +23,7 @@ void *rd_malloc_or_die(size_t size)
 {
 	void *r = malloc(size);
 	if (!r)
-		rd_fatal("malloc: allocating %lu bytes: %m",
-		         (unsigned long) size);
+		rd_fatal("malloc: allocating %zu bytes: %m", size);
 	return r;
 }
 