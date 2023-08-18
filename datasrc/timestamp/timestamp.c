4e1d532e0662b4a19a2de4f328addaa066f7ce46
Alexey Gladkov <gladkov.alexey@gmail.com>
2023-08-18 16:03:55 +0200

    Use really raw output
    Leading zeros can interfere with the use of values in bash. bash tries
    to convert it and fails.
    
    + now=000008.562075087
    + now=000008
    + deadline=182
    + $(( $deadline - $now ))
    line 60: 182 - 000008: value too great for base (error token is "000008")
    
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- datasrc/timestamp/timestamp.c
+++ datasrc/timestamp/timestamp.c
@@ -10,7 +10,7 @@ main(int argc, char **argv)
 	struct timespec tp = { 0 };
 
 	if (arg == argc) {
-		fprintf(stderr, "Usage: timestamp {monotonic|boottime} [raw]\n");
+		fprintf(stderr, "Usage: timestamp {monotonic|boottime} [raw|simple]\n");
 		return 0;
 	} else if (!strcmp(argv[arg], "monotonic")) {
 		clockid = CLOCK_MONOTONIC_RAW;
@@ -24,9 +24,15 @@ main(int argc, char **argv)
 
 	clock_gettime(clockid, &tp);
 
-	if (arg < argc && !strcmp(argv[arg], "raw")) {
-		printf("%06lu.%07lu", (unsigned long) tp.tv_sec, tp.tv_nsec);
-		return 0;
+	if (arg < argc) {
+		if (!strcmp(argv[arg], "simple")) {
+			printf("%06lu.%07lu", (unsigned long) tp.tv_sec, tp.tv_nsec);
+			return 0;
+		}
+		if (!strcmp(argv[arg], "raw")) {
+			printf("%lu.%lu", (unsigned long) tp.tv_sec, tp.tv_nsec);
+			return 0;
+		}
 	}
 	printf("[%5lu.%06lu] ", (unsigned long) tp.tv_sec, tp.tv_nsec / 1000);
 	return 0;
