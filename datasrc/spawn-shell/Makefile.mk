348666e130f33603a44c2b1a76e86b2def72d989
Alexey Gladkov <gladkov.alexey@gmail.com>
2024-01-20 12:18:57 +0000

    Remove spawn-shell
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- datasrc/spawn-shell/Makefile.mk
+++
@@ -1,7 +0,0 @@
-spawn_shell_DEST = $(dest_data_bindir)/spawn-shell
-spawn_shell_SRCS = datasrc/spawn-shell/spawn-shell.c
-
-spawn_shell_CFLAGS = -D_GNU_SOURCE -Idatasrc/libinitramfs
-spawn_shell_LIBS = -L$(dest_data_libdir) -linitramfs
-
-PROGS += spawn_shell
