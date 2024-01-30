c9b578c842ed9650d8f901d34b1f467a64d1569a
Alexey Gladkov <gladkov.alexey@gmail.com>
2024-01-30 17:40:14 +0000

    Make System.map optional
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/depmod-image/rules.mk
+++ features/depmod-image/rules.mk
@@ -3,7 +3,8 @@
 
 depmod-image: create
 	@$(VMSG) "Generating module dependencies in image ..."
-	$Q/sbin/depmod -a -F "$(BOOTDIR)/System.map-$(KERNEL)" -b $(ROOTDIR) \
-		"$(KERNEL)"
+	$Qsystem_map_file="$(BOOTDIR)/System.map-$(KERNEL)"; \
+	[ -e "$$system_map_file" ] || system_map_file=""; \
+	/sbin/depmod -a $${system_map_file:+-F "$$system_map_file"} -b $(ROOTDIR) "$(KERNEL)"
 
 pack: depmod-image
