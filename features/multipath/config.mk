d5c36c5a36448b5af2f889bf231c181bd7cc1672
Alexey Gladkov <gladkov.alexey@gmail.com>
2024-01-31 14:49:33 +0000

    feature/multipath: Logically separate the multipath and sg3_utils files
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/multipath/config.mk
+++ features/multipath/config.mk
@@ -1,6 +1,21 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 $(call feature-requires,depmod-image add-udev-rules devmapper)
 
+# Summary: Utilities for devices that use SCSI command sets
+# URL: https://sg.danny.cz/sg/sg3_utils.html
+MULTIPATH_SG3_PROGS_PATTERNS = \
+	*/sg_inq
+
+MULTIPATH_SG3_RULES = \
+	*-fc-wwpn-id.rules \
+	*-scsi-sg3*.rules \
+	*-usb-blacklist.rules
+
+MULTIPATH_SG3_FILES = \
+	$(wildcard /lib/udev/fc_wwpn_id)
+
+# Summary: Tools to manage multipath devices with device-mapper
+# URL: http://christophe.varoqui.free.fr
 MULTIPATH_PRELOAD = \
 	dm-multipath \
 	dm-queue-length \
@@ -10,27 +25,29 @@ MULTIPATH_PRELOAD = \
 	scsi_dh_emc \
 	scsi_dh_rdac
 
-MULTIPATH_PROGS   = multipathd multipath kpartx sg_inq
+MULTIPATH_PROGS = multipathd multipath kpartx
+MULTIPATH_PROGS_PATTERNS = $(MULTIPATH_SG3_PROGS_PATTERNS)
+
 MULTIPATH_CONF    = \
 	$(SYSCONFDIR)/multipath.conf \
 	$(SYSCONFDIR)/multipath \
 	$(SYSCONFDIR)/multipath/bindings \
 	$(SYSCONFDIR)/multipath/wwids
+
 MULTIPATH_DATADIR = \
 	$(FEATURESDIR)/multipath/data
+
 MULTIPATH_RULES   = \
 	*-dm-mpath.rules \
 	*-dm-parts.rules \
-	*-fc-wwpn-id.rules \
 	*-kpartx.rules \
 	*-multipath.rules \
-	*-scsi-sg3*.rules \
-	*-usb-blacklist.rules
+	$(MULTIPATH_SG3_RULES)
 
 ifeq "$(MULTIPATH_PLUGINS_DIRS)" ""
 MULTIPATH_PLUGINS_DIRS = $(foreach d,$(LIB_DIRS),$(wildcard $(d)/multipath))
 endif
 
 MULTIPATH_FILES = \
-	/lib/udev/fc_wwpn_id \
+	$(MULTIPATH_SG3_FILES) \
 	$(MULTIPATH_PLUGINS_DIRS)
