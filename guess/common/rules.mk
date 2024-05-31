e4fc9f0cec110d78928c8344987e38f8e55e4dd9
Alexey Gladkov <gladkov.alexey@gmail.com>
2024-05-31 11:54:23 +0200

    Add trace functions for guess subsystem
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- guess/common/rules.mk
+++ guess/common/rules.mk
@@ -5,8 +5,6 @@ GUESS_COMMON_SCRIPTS = $(call wildcard-features,guess/common)
 
 guess-common:
 	$V echo "Processing $@ ..."
-	@for m in $(GUESS_COMMON_SCRIPTS); do \
-	    [ ! -x "$$m" ] || "$$m"; \
-	done
+	@ $(DETECTDIR)/common/action
 
 guess: guess-common
