bea573ac60bcbcd4f38a806987a7c6a307acbaae
Alexey Gladkov <gladkov.alexey@gmail.com>
2024-12-10 16:34:51 +0100

    feature/fsck: Add workaround for altlinux where fsck has non upstream compatible options
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/fsck/rules.mk
+++ features/fsck/rules.mk
@@ -3,3 +3,12 @@ FSCK_RUNTIME_FILES := $(shell $(call shell-export-vars) $(FEATURESDIR)/fsck/bin/
 
 PUT_FEATURE_DIRS  += $(FSCK_DATADIR)
 PUT_FEATURE_FILES += $(FSCK_FILES) $(FSCK_RUNTIME_FILES)
+
+VENDOR =
+ifneq "$(wildcard /etc/os-release)" ""
+  VENDOR = $(shell sed -n -e 's/^ID=//p' /etc/os-release)
+endif
+
+ifeq "$(VENDOR)" "altlinux"
+$(call set-sysconfig,fsck,FSCK_ARGS,-py)
+endif
