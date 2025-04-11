osboot/make-initrd

commit e61089c04ad8708c0866d944fce589cac5ce78a9

enqueue-library.c: Use libc NT_FDO_PACKAGING_METADATA definition
Use the NT_FDO_PACKAGING_METADATA macro provided by glibc headers,
available since commit glibc-2.35~294 ("elf: add definition for
ELF_NOTE_FDO and NT_FDO_PACKAGING_METADATA note").  Retain the fallback
definition for compatibility with older glibc and other libc
implementations.

Signed-off-by: Gabi Falk <gabifalk@gmx.com>

utils/initrd-put/enqueue-library.c
@@ -18,7 +18,9 @@
 #include "enqueue.h"
 #include "elf_dlopen.h"
 
-#define _FDO_ELF_METADATA 0x407c0c0a
+#ifndef NT_FDO_PACKAGING_METADATA
+# define NT_FDO_PACKAGING_METADATA 0x407c0c0a
+#endif
 
 extern int verbose;
 
@@ -117,7 +119,9 @@ int enqueue_elf_dlopen(const char *filename, int fd)
 
 		off = 0;
 		while ((next = gelf_getnote(data, off, &nhdr, &n_off, &d_off)) > 0) {
-			if (nhdr.n_type == _FDO_ELF_METADATA && nhdr.n_namesz == sizeof(ELF_NOTE_FDO) && !strcmp(data->d_buf + n_off, ELF_NOTE_FDO)) {
+			if (nhdr.n_type == NT_FDO_PACKAGING_METADATA &&
+			    nhdr.n_namesz == sizeof(ELF_NOTE_FDO) &&
+			    !strcmp(data->d_buf + n_off, ELF_NOTE_FDO)) {
 				library[0] = '\0';
 
 				process_json_metadata(filename, (char *)data->d_buf + d_off, library);

commit 834b371dcbc219941adfe726ed1ea729d1707527

enqueue-library.c: Use NT_FDO_DLOPEN_METADATA instead of NT_FDO_PACKAGING_METADATA
The NT_FDO_DLOPEN_METADATA section must be used to specify dependencies
in the elf section. This was changed by accident and resulted in a
complete breakdown of the dependency search functionality.

Fixes: e61089c0 ("enqueue-library.c: Use libc NT_FDO_PACKAGING_METADATA definition")
Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

utils/initrd-put/enqueue-library.c
@@ -17,8 +17,8 @@
 #include "enqueue.h"
 #include "elf_dlopen.h"
 
-#ifndef NT_FDO_PACKAGING_METADATA
-# define NT_FDO_PACKAGING_METADATA 0x407c0c0a
+#ifndef NT_FDO_DLOPEN_METADATA
+# define NT_FDO_DLOPEN_METADATA 0x407c0c0a
 #endif
 
 #ifndef ELF_NOTE_FDO
@@ -122,7 +122,7 @@ int enqueue_elf_dlopen(const char *filename, int fd)
 
 		off = 0;
 		while ((next = gelf_getnote(data, off, &nhdr, &n_off, &d_off)) > 0) {
-			if (nhdr.n_type == NT_FDO_PACKAGING_METADATA &&
+			if (nhdr.n_type == NT_FDO_DLOPEN_METADATA &&
 			    nhdr.n_namesz == sizeof(ELF_NOTE_FDO) &&
 			    !strcmp(data->d_buf + n_off, ELF_NOTE_FDO)) {
 				library[0] = '\0';