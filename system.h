osboot/make-initrd

commit 7929630e2f93a6e56cfd507f79e29aa6cd3c4380

*.c: Include config.h header
The config.h header is generated by the configure script and should be
included before standard library headers to correctly enable features
such as large file support or time64 on 32-bit systems.

Signed-off-by: Gabi Falk <gabifalk@gmx.com>

system.h
@@ -1,0 +1,1 @@
+#include "config.h"

features/sshfsroot/src/ask-pass/ask-pass.c
@@ -1,4 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "config.h"
+
 #include <stdio.h>
 #include <termios.h>
 #include <unistd.h>

runtime/src/chrooted/chrooted.c
@@ -1,4 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "config.h"
+
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdlib.h>

runtime/src/environ/environ.c
@@ -1,4 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "config.h"
+
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/stat.h>

runtime/src/halt/halt.c
@@ -31,6 +31,8 @@
  *		Copyright (C) 1991-2004 Miquel van Smoorenburg.
  */
 
+#include "config.h"
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/param.h>

runtime/src/halt/hddown.c
@@ -7,6 +7,8 @@
  */
 const char *v_hddown = "@(#)hddown.c  1.02  22-Apr-2003  miquels@cistron.nl";
 
+#include "config.h"
+
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
 #endif

runtime/src/halt/ifdown.c
@@ -7,6 +7,8 @@
  */
 const char *v_ifdown = "@(#)ifdown.c  1.11  02-Jun-1998  miquels@cistron.nl";
 
+#include "config.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>

runtime/src/libinitramfs/console.c
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#include "config.h"
+
 #include <linux/kd.h>
 #include <sys/ioctl.h>
 

commit ed03991b340ec4bff30674d2a0810a396ba75a23

Remove unused header
Fixes: 7929630e ("*.c: Include config.h header")
Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

system.h
@@ -1,1 +1,0 @@
-#include "config.h"