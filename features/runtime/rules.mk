cf2e7f1f9ba9ba63fd1128d92dff8bd6ddcf589f
Alexey Gladkov <gladkov.alexey@gmail.com>
2025-04-16 17:15:42 +0200

    feature/runtime: Move hardcoded utilities to feature
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/runtime/rules.mk
+++ features/runtime/rules.mk
@@ -2,6 +2,17 @@
 
 PHONY += create
 
+PUT_FEATURE_PROGS += \
+	blkid cat chmod chroot cp depmod dmesg findmnt grep kill killall5 ln \
+	logger ls lsmod mkdir mknod mktemp modprobe mount mountpoint mv \
+	readlink rm rmdir setsid sh sleep start-stop-daemon switch_root \
+	touch umount which
+
+PUT_FEATURE_PROGS_WILDCARD = \
+	*/shell-*
+
+PUT_FEATURE_FILES += $(UDEVD) $(UDEVADM)
+
 PUT_FEATURE_DIRS += $(FEATURESDIR)/runtime/data
 
 create: depmod-host
