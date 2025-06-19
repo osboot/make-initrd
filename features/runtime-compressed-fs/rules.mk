a929ec7c26e14b4aef6750621afde23f9981ac08
Alexey Gladkov <gladkov.alexey@gmail.com>
2025-06-19 15:34:56 +0200

    Show information about packed modules in all images
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/runtime-compressed-fs/rules.mk
+++ features/runtime-compressed-fs/rules.mk
@@ -3,6 +3,12 @@
 RUNTIME_LAYER_SQUASHFS_MODULES := overlay loop squashfs
 RUNTIME_LAYER_EROFS_MODULES    := overlay loop erofs
 
+RUNTIME_COMPRESSED_FS := compressed-fs
+
+GARBAGE += \
+	$(ROOTDIR).$(RUNTIME_COMPRESSED_FS) \
+	$(WORKDIR)/$(RUNTIME_COMPRESSED_FS)
+
 pre-pack::
 	@$(VMSG) "Creating squashfs image ..."
 	@$(FEATURESDIR)/runtime-compressed-fs/bin/pack-compressed-fs
