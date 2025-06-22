23fcf0f06685e6bd7a0ae5375a2061c604e10434
Alexey Gladkov <gladkov.alexey@gmail.com>
2025-06-22 14:57:13 +0200

    guess: Drop loaded-modules feature
    This feature was needed as a workaround to the problem of not being able
    to parse Device Tree (DT). Now that there is a separate device-tree
    feature, there is no need for loaded-modules.
    
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- guess/loaded-modules/rules.mk
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: GPL-3.0-or-later
-PHONY += guess-loaded-modules
-
-guess-loaded-modules:
-	$V echo "Processing $@ ..."
-	@ GUESS_SUFFIX=add:$@ \
-	    $(DETECTDIR)/loaded-modules/action
-
-guess: guess-loaded-modules
