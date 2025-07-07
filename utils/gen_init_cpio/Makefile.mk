aab9b26cc96dc299fbf2842f131d51ab9dd5ea83
Alexey Gladkov <gladkov.alexey@gmail.com>
2025-07-07 15:04:18 +0200

    Replace gen_init_cpio by cpio
    The gen_init_cpio utility has a fundamental defect - the format does not
    support spaces in filenames. Fixing this would require a major
    modification.
    
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- utils/gen_init_cpio/Makefile.mk
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-gen_init_cpio_DEST = $(dest_bindir)/gen_init_cpio
-gen_init_cpio_SRCS = $(utils_srcdir)/gen_init_cpio/gen_init_cpio.c
-gen_init_cpio_LIBS =
-gen_init_cpio_CFLAGS = -Wno-sign-conversion -Wno-discarded-qualifiers
-
-PROGS += gen_init_cpio
