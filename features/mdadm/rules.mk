8a4a2f310608b4fe68f217e3b17b59cfc5839766
Alexey Gladkov <gladkov.alexey@gmail.com>
2025-07-14 11:08:17 +0200

    feature/mdadm: Move GENERATE_UDEV_RULES_FOR_MD_DEVICE processing inside the helper
    To avoid many forks from rules.mk, let's move GENERATE_UDEV_RULES_FOR_MD_DEVICE
    processing to the generate-udev-rules script. This will allow all the majmin
    to be processed at once.
    
    Another benefit from the change is the separation between udev rule
    generation and rootfs data generation.
    
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/mdadm/rules.mk
+++ features/mdadm/rules.mk
@@ -1,10 +1,8 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
-MDADM_UDEV_RULES := \
-	$(foreach majmin,$(GENERATE_UDEV_RULES_FOR_MD_DEVICE),\
-		$(shell $(call shell-export-vars) $(FEATURESDIR)/mdadm/bin/generate-udev-rules $(majmin)))
+MDADM_UDEV_RULES := $(shell $(call shell-export-vars) $(FEATURESDIR)/mdadm/bin/generate-udev-rules)
+MDADM_DIRS       := $(shell $(call shell-export-vars) $(FEATURESDIR)/mdadm/bin/generate-udev-rules dirs)
 
-PUT_UDEV_RULES += $(MDAMD_RULES_OLD) $(MDAMD_RULES) $(MDADM_UDEV_RULES)
-
-PUT_FEATURE_DIRS  += $(MDADM_DATADIR)
+PUT_UDEV_RULES    += $(MDAMD_RULES_OLD) $(MDAMD_RULES) $(MDADM_UDEV_RULES)
+PUT_FEATURE_DIRS  += $(MDADM_DATADIR) $(MDADM_DIRS)
 PUT_FEATURE_FILES += $(MDADM_FILES)
 PUT_FEATURE_PROGS += $(MDADM_PROGS) $(MDADM_EXTRA_PROGS)
