osboot/make-initrd

commit a8b613f15dfe231e92a590e3e977f6bec9199f1b

features: Run shell code only once
After the introduction of packimage.mk, the execution of shell halpers
started to happen twice, since genimage.mk still calls make and
reimports rule.mk.

Therefore, the previous design no longer works:

FINAL_VAR := $(shell ...)
PUT_FEATURE_FILES += $(FINAL_VAR)

When make is restarted, the := operation will be executed again.

The new feature takes this into account and avoids having to execute
shell code for every make sub-run.

Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

features/plymouth/rules.mk
@@ -1,9 +1,11 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 PLYMOUTH_DIRS := $(foreach d,$(LIB_DIRS),$(wildcard $(d)/plymouth))
 
-PLYMOUTH_PACK_MODULES := $(shell $(call shell-export-vars) $(FEATURESDIR)/plymouth/bin/get-modules)
-PLYMOUTH_PACK_FONT    := $(or $(PLYMOUTH_FONT),$(shell fc-match -f '%{file}\n' 'DejaVuSans' ||:))
-PLYMOUTH_PACK_THEME   := $(shell $(call shell-export-vars) $(FEATURESDIR)/plymouth/bin/get-theme-files "$(PLYMOUTH_THEME)")
+$(call assgin-shell-once,PLYMOUTH_PACK_MODULES, $(FEATURESDIR)/plymouth/bin/get-modules)
+$(call assgin-shell-once,PLYMOUTH_PACK_THEME,   $(FEATURESDIR)/plymouth/bin/get-theme-files "$(PLYMOUTH_THEME)
+$(call assgin-shell-once,PLYMOUTH_PACK_DEFFONT, fc-match -f '%{file}\n' 'DejaVuSans' ||:)
+
+PLYMOUTH_PACK_FONT := $(or $(PLYMOUTH_FONT),$(PLYMOUTH_PACK_DEFFONT))
 
 PLYMOUTH_EXCLUDE_MODULES   ?=
 PLYMOUTH_EXCLUDE_RENDERERS ?= %/x11.so

features/fsck/rules.mk
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
-FSCK_RUNTIME_FILES := $(shell $(call shell-export-vars) $(FEATURESDIR)/fsck/bin/find-files)
+
+$(call assgin-shell-once,FSCK_RUNTIME_FILES,$(FEATURESDIR)/fsck/bin/find-files)
 
 PUT_FEATURE_DIRS  += $(FSCK_DATADIR)
 PUT_FEATURE_FILES += $(FSCK_FILES) $(FSCK_RUNTIME_FILES)

features/gpu-drm/rules.mk
@@ -1,4 +1,5 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
-GPUDRM_MODULES := $(shell $(call shell-export-vars) $(FEATURESDIR)/gpu-drm/bin/get-modules)
+
+$(call assgin-shell-once,GPUDRM_MODULES,$(FEATURESDIR)/gpu-drm/bin/get-modules)
 
 MODULES_ADD += $(GPUDRM_MODULES)

features/kickstart/rules.mk
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
-KICKSTART_DIRS := $(shell $(call shell-export-vars) $(FEATURESDIR)/kickstart/bin/get-ks-configs)
+
+$(call assgin-shell-once,KICKSTART_DIRS,$(FEATURESDIR)/kickstart/bin/get-ks-configs)
 
 FAT_MODULES = fs-vfat /fs/nls/
 ZFS_MODULES = zfs

features/luks/rules.mk
@@ -2,7 +2,7 @@
 LUKS_CRYPTTAB_DATA =
 
 ifneq ($(LUKS_CRYPTTAB),)
-LUKS_CRYPTTAB_DATA := $(shell $(call shell-export-vars) $(FEATURESDIR)/luks/bin/get-data)
+$(call assgin-shell-once,LUKS_CRYPTTAB_DATA,$(FEATURESDIR)/luks/bin/get-data)
 endif
 
 MODULES_LOAD += $(LUKS_MODULES)

features/mdadm/rules.mk
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
-MDADM_UDEV_RULES := $(shell $(call shell-export-vars) $(FEATURESDIR)/mdadm/bin/generate-udev-rules)
-MDADM_DIRS       := $(shell $(call shell-export-vars) $(FEATURESDIR)/mdadm/bin/generate-udev-rules dirs)
+
+$(call assgin-shell-once,MDADM_UDEV_RULES,$(FEATURESDIR)/mdadm/bin/generate-udev-rules)
+$(call assgin-shell-once,MDADM_DIRS,      $(FEATURESDIR)/mdadm/bin/generate-udev-rules dirs)
 
 PUT_UDEV_RULES    += $(MDAMD_RULES_OLD) $(MDAMD_RULES) $(MDADM_UDEV_RULES)
 PUT_FEATURE_DIRS  += $(MDADM_DATADIR) $(MDADM_DIRS)

features/rdshell/rules.mk
@@ -5,7 +5,7 @@ endif
 
 ifneq ($(RDSHELL_PASSWORD)$(RDSHELL_COPY_PASSWORD_FROM_USER),)
 RDSHELL = login
-RHSHELL_DIRS := $(shell $(call shell-export-vars) $(FEATURESDIR)/rdshell/bin/add-login dirs)
+$(call assign-shell-once,RHSHELL_DIRS,$(FEATURESDIR)/rdshell/bin/add-login dirs)
 PUT_FEATURE_DIRS += $(RHSHELL_DIRS)
 endif
 

features/rootfs/rules.mk
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
-ROOTFS_DIRS := $(shell $(call shell-export-vars) $(FEATURESDIR)/rootfs/bin/create-fstab dirs)
+
+$(call assgin-shell-once,ROOTFS_DIRS,$(FEATURESDIR)/rootfs/bin/create-fstab dirs)
+
 PUT_FEATURE_DIR += $(ROOTFS_DIRS)
 
 $(call set-sysconfig,init,QUIET,$(RUNTIME_QUIET))

commit 226e74ecb57c1c7798d8896a0ddb378658ca9819

feature/plymouth: Fix syntax error
During refactoring, the end of the line was removed.

Fixes: a8b613f1 ("features: Run shell code only once")
Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

features/plymouth/rules.mk
@@ -2,7 +2,7 @@
 PLYMOUTH_DIRS := $(foreach d,$(LIB_DIRS),$(wildcard $(d)/plymouth))
 
 $(call assign-shell-once,PLYMOUTH_PACK_MODULES, $(FEATURESDIR)/plymouth/bin/get-modules)
-$(call assign-shell-once,PLYMOUTH_PACK_THEME,   $(FEATURESDIR)/plymouth/bin/get-theme-files "$(PLYMOUTH_THEME)
+$(call assign-shell-once,PLYMOUTH_PACK_THEME,   $(FEATURESDIR)/plymouth/bin/get-theme-files "$(PLYMOUTH_THEME)")
 $(call assign-shell-once,PLYMOUTH_PACK_DEFFONT, fc-match -f '%{file}\n' 'DejaVuSans' ||:)
 
 PLYMOUTH_PACK_FONT := $(or $(PLYMOUTH_FONT),$(PLYMOUTH_PACK_DEFFONT))