d4cfad7ab30f0fa6e01614303972013539196a3e
Alexey Gladkov <gladkov.alexey@gmail.com>
2025-09-25 23:40:16 +0200

    feature/modules-virtio: Fix symbol filter
    After linux kernel commit ffe6176b7f53 ("virtio: store owner from
    modules with register_virtio_driver()"), the symbol name changed and no
    longer matches the regular expression.
    
    Link: https://github.com/torvalds/linux/commit/ffe6176b7f53ca0c99355f13e14a33a40cf49406
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- features/modules-virtio/rules.mk
+++ features/modules-virtio/rules.mk
@@ -1,3 +1,3 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
-VIRTIO_PATTERN_SET = symbol:^register_virtio_driver
+VIRTIO_PATTERN_SET = symbol:^(__)?register_virtio_driver
 MODULES_PATTERN_SETS += VIRTIO_PATTERN_SET
